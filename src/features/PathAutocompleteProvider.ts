748e56954885f35507a82ffc3b50e82e3f3230cf
Nedim Filipovic <nedim.filipovic@payclearly.com>
2019-01-04 16:04:52 -0800

    First commit, scaffolded out extension

---
+++ src/features/PathAutocompleteProvider.ts
@@ -0,0 +1,9 @@
+import * as vs from 'vscode';
+
+// node modules
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class PathAutocomplete implements vs.CompletionItemProvider {
+
+}
